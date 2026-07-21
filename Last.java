b2ff2f1d7864dc62a0aa943372a81229914cb2b6
mariyamthahura07-web <mariyamthahura07@gmail.com>
2026-07-21 09:05:57 +0530

    Add files via upload

---
+++ Last.java
@@ -0,0 +1,13 @@
+public class Last {
+    public int lengthOfLastWord(String s) {
+        String[] words = s.trim().split("\\s+");
+
+        if (words.length > 0) {
+            return words[words.length - 1].length();
+        } else {
+            return 0;
+
+        }
+
+    }
+}
